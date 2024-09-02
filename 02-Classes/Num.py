1f7b307b7fb8c5143d539743b4fa178e87572dc0
Joe Hall Lee <sacredkayma@qq.com>
2024-09-02 20:32:29 +0800

    Add files via upload.

---
+++ 02-Classes/Num.py
@@ -0,0 +1,13 @@
+num1 = 11
+
+num2 = num1
+
+print("Before value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+num1 = 22
+
+print("\nAfter value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
